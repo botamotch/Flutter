f1aeadcb9669f3f7f96f56b1e0148e1284572c69
botamotch <laughing00boomer@gmail.com>
2022-01-24 21:07:08 +0900

    数独アプリ作成中。パネル表示して反応するところまで

--- positioned_test_app/lib/panel.dart
+++ positioned_test_app/lib/panel.dart
@@ -1,113 +1,144 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 class GameAreaWidget extends StatelessWidget {
   const GameAreaWidget({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Expanded(child: PanelContainerWidget()),
-            MenuWidget(),
-          ],
-        ),
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(child: PanelContainerWidget()),
+          Expanded(child: MenuWidget()),
+          // PanelContainerWidget(),
+          // MenuWidget(),
+        ],
       ),
     );
   }
 }
 
-class PanelContainerWidget extends StatelessWidget {
-  const PanelContainerWidget({Key? key}) : super(key: key);
-  void _initNumber() {}
+class MenuWidget extends StatefulWidget {
+  const MenuWidget({Key? key}) : super(key: key);
+  @override
+  State<MenuWidget> createState() => _MenuWidgetState();
+}
+
+class _MenuWidgetState extends State<MenuWidget> {
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
-    double height = MediaQuery.of(context).size.height;
+    List<Positioned> _positionList = [
+      for (int i = 0; i < 9; i += 1)
+        Positioned(
+            top: 10,
+            left: width / 9 * (i as double),
+            width: width / 9,
+            height: width / 9,
+            child: GestureDetector(
+              onTap: () {},
+              child: Container(
+                alignment: Alignment(0.0, 0.0),
+                // color: Colors.blueGrey,
+                child: Text('$i'),
+              ),
+            ))
+    ];
     return Stack(
       fit: StackFit.expand,
-      children: [for (var i = 0; i < 81; i += 1) i]
-          .map((int value) => PanelContentWidget(index: value, size: width))
-          .toList(),
+      children: _positionList,
     );
+    // return Container(child: Text('hello'));
   }
 }
 
-class MenuWidget extends StatelessWidget {
-  const MenuWidget({Key? key}) : super(key: key);
+class PanelContainerWidget extends StatefulWidget {
+  const PanelContainerWidget({Key? key}) : super(key: key);
+
+  static List<int> correctNumber = [
+    for (int i = 0; i < 81; i += 1) Random().nextInt(9) + 1
+  ];
+  static List<int> inputNumber = [];
+  static List<bool> visible = [];
+
   @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
+  State<PanelContainerWidget> createState() => _PanelContainerWidgetState();
 }
 
-class PanelContentWidget extends StatefulWidget {
-  const PanelContentWidget({Key? key, required this.index, required this.size})
-      : super(key: key);
-  final int index;
-  final double size;
-  @override
-  State<PanelContentWidget> createState() => _PanelContentWidgetState();
-}
+class _PanelContainerWidgetState extends State<PanelContainerWidget> {
+  List<PanelContentWidget> panelList = [for (var i = 0; i < 81; i += 1) i]
+      .map((int i) => PanelContentWidget(
+            color: Colors.white70,
+            number: PanelContainerWidget.correctNumber[i],
+          ))
+      .toList();
 
-class _PanelContentWidgetState extends State<PanelContentWidget> {
-  int _correctNumber = 0;
-  int _inputNumber = 0;
-  int _visualizedNumber = 0;
-  Color _color = Colors.white70;
-
-  void _PanelTapped() {
+  void _initNumber() {
     setState(() {
-      _color = Colors.black12;
+      panelList = [for (int i = 0; i < 81; i += 1) i]
+          .map((int i) => PanelContentWidget(
+                color: Colors.white70,
+                number: PanelContainerWidget.correctNumber[i],
+              ))
+          .toList();
+    });
+  }
+
+  void _resetColor(int index) {
+    setState(() {
+      panelList = [for (int i = 0; i < 81; i += 1) i]
+          .map((int i) => PanelContentWidget(
+                color: (i != index) ? Colors.white70 : Colors.black12,
+                number: PanelContainerWidget.correctNumber[i],
+              ))
+          .toList();
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Positioned(
-      top: widget.size / 9 * ((widget.index ~/ 9) as double),
-      left: widget.size / 9 * ((widget.index % 9) as double),
-      width: widget.size / 9,
-      height: widget.size / 9,
-      child: GestureDetector(
-        onTap: _PanelTapped,
-        child: Container(
-          alignment: Alignment(0.0, 0.0),
-          color: _color,
-          child: Text('${_visualizedNumber}'),
+    double width = MediaQuery.of(context).size.width;
+    // double height = MediaQuery.of(context).size.height;
+    List<Positioned> _positionList = [];
+    panelList.asMap().forEach((int i, PanelContentWidget p) {
+      _positionList.add(Positioned(
+        top: width / 9 * ((i ~/ 9) as double),
+        left: width / 9 * ((i % 9) as double),
+        width: width / 9,
+        height: width / 9,
+        child: GestureDetector(
+          onTap: () {
+            _resetColor(i);
+          },
+          child: p,
         ),
-      ),
+      ));
+    });
+    _initNumber();
+
+    return Stack(
+      fit: StackFit.expand,
+      children: _positionList,
     );
   }
 }
 
-/* ========================================================================== */
+class PanelContentWidget extends StatelessWidget {
+  const PanelContentWidget(
+      {Key? key, required this.color, required this.number})
+      : super(key: key);
 
-// class PanelWidget extends StatefulWidget {
-//   const PanelWidget({Key? key, required this.number, required this.size})
-//       : super(key: key);
-//   final int number;
-//   final Color color = Colors.white70;
-//   final double size;
-//
-//   @override
-//   State<PanelWidget> createState() => _PanelWidgetState();
-// }
+  final int number;
+  final Color color;
 
-// class _PanelWidgetState extends State<PanelWidget> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Positioned(
-//       top: 10 + widget.size / 9 * ((widget.number ~/ 9) as double),
-//       left: 10 + widget.size / 9 * ((widget.number % 9) as double),
-//       width: widget.size / 9,
-//       height: widget.size / 9,
-//       child: Container(
-//         alignment: Alignment(0.0, 0.0),
-//         color: widget.color,
-//         child: Text('${widget.number}'),
-//       ),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment(0.0, 0.0),
+      color: color,
+      child: Text('$number'),
+    );
+  }
+}
